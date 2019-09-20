58d552c5fe7901e40d17008596a5cc945b39f33d
Debra Hodges <dhodges351@gmail.com>
2019-09-20 13:31:36 -0500

    Remove dist folder and large files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var path = require('path');
 
-var port = process.env.PORT || 4200;
+var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
